5bc4b84a750b0627e8568ec59e9aec8f5cedbcb5
Egemen <egemenwasd@gmail.com>
2022-05-12 22:48:22 +0300

    #new method  and instance variable created for chosen flight params to view in bookings/new

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,12 @@
 class BookingsController < ApplicationController
   def new
+    @chosen_flight_params = params
+    @booking = Booking.new
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, :passenger_id)
   end
 end
