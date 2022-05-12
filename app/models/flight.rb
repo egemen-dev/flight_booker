d46463898bf01ae77134ae76b50703ae82017798
Egemen <egemenwasd@gmail.com>
2022-05-12 22:51:44 +0300

    active record associations set up betwen booking-flight-passenger models

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,6 @@
 class Flight < ApplicationRecord
+    has_many :bookings, foreign_key: :flight_id, class_name: 'Booking'
+    has_many :passengers, through: :bookings
     belongs_to :departure, class_name: 'Airport'
     belongs_to :arrival, class_name: 'Airport'
 end
