1b20724e51f59bfb1c2969993061225f8cc6ba8f
Egemen <egemenwasd@gmail.com>
2022-05-13 15:24:51 +0300

    booking controller methods set up and nested parameters allowed

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,11 +2,27 @@ class BookingsController < ApplicationController
   def new
     @chosen_flight_params = params
     @booking = Booking.new
+    pass_num = @chosen_flight_params[:passenger_number].to_i
+    pass_num.times { @booking.passengers.build }
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      flash[:notice] = "You have successfully book the flight."
+      redirect_to booking_path(@booking)
+    else
+      flash[:error] = "Try again!"
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
   end
 
   private
 
   def booking_params
-    params.require(:booking).permit(:flight_id, :passenger_id)
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:name, :email])
   end
 end
