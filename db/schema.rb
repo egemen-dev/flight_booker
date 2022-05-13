74ad0743ccb0f3bbc6ce67c8d1b9c93c1b63d2d6
Egemen <egemenwasd@gmail.com>
2022-05-13 15:20:09 +0300

    passenger_id column removed from booking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_12_103703) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_13_102515) do
   create_table "airports", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -20,7 +20,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_12_103703) do
 
   create_table "bookings", force: :cascade do |t|
     t.integer "flight_id"
-    t.integer "passenger_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -40,6 +39,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_12_103703) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "booking_id"
   end
 
 end
