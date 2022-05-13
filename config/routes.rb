134db10c91b7e9238beba4d39c807e9461d700f6
Egemen <egemenwasd@gmail.com>
2022-05-13 15:20:42 +0300

    booking routes are added

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'bookings/new'
+  resources :bookings, only: [:new, :create, :show]
   get 'flights/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "flights#index"
 end
